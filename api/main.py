d2521ae1cbd6bbdd47109367f6009d31030957ae
Alberto Fleitas <betofleitascardus@hotmail.com>
2022-10-02 16:59:38 -0300

    docs: update open api schema

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 from fastapi import (FastAPI, status)
+from fastapi.openapi.utils import get_openapi
+
 
 from routers import users_router, tweets_router
 from sql import models
@@ -13,9 +15,25 @@ app.include_router(users_router.router)
 app.include_router(tweets_router.router)
 
 
-@app.get(
-    path="/",
-    status_code=status.HTTP_200_OK,
-    tags=["Home"])
-async def home():
-    return {"Hello": "World"}
+# Customize open api schema
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Twitter API",
+        version="1.0",
+        description=""" ### This is a Twitter API project. You can create, read, update and delete Users and Tweets.
+        \n  - ## [Project's repository on GitHub](https://github.com/betofleitass/twitter-fast-api/)
+        \n  - ## [Contact me](https://www.linkedin.com/in/fleitas-alberto/)""",
+        routes=app.routes,
+        license_info={"name": "License: MIT",
+                      "url": "https://choosealicense.com/licenses/mit/"}
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
